4a16d08b79a81d7de4c5959b87ccfd10ff172968
AudioStakes <daisuke.sato@enishi-tech.com>
2021-07-15 17:50:59 +0900

    メモの編集機能を追加

--- app.rb
+++ app.rb
@@ -46,6 +46,47 @@ post '/memos' do
   end
 end
 
+get %r{/memos/(\d+)/edit} do
+  target_id = params['captures'].first
+  csv = CSV.read(CSV_FILE_PATH, encoding: 'bom|utf-8', headers: true, header_converters: :symbol)
+  @memo = csv.find { |row| row[:id] == target_id }
+
+  if @memo
+    haml :'memos/edit'
+  else
+    error 404
+  end
+end
+
+patch %r{/memos/(\d+)} do
+  target_id = params['captures'].first
+  title = request['title']
+  content = request['content']
+
+  if title && content
+    csv = CSV.read(CSV_FILE_PATH, encoding: 'bom|utf-8', headers: true, header_converters: :symbol)
+
+    if (target_row = csv.find { |row| row[:id] == target_id })
+      csv.delete_if { |row| row[:id] == target_id }
+
+      target_row[:title] = title
+      target_row[:content] = content
+
+      csv << target_row
+
+      # sort した方が良いかも？
+
+      File.open(CSV_FILE_PATH, 'w') do |f|
+        f.write(csv.to_csv)
+      end
+    end
+
+    redirect :'memos/index'
+  else
+    error 400 # より適したステータスコードがあるかも？
+  end
+end
+
 delete %r{/memos/(\d+)} do
   csv = CSV.read(CSV_FILE_PATH, encoding: 'bom|utf-8', headers: true, header_converters: :symbol)
 
